9b419cdcba6e05d5e0f8e78f19ac7cf9805feaa2
Sam Stevens <samdjstevens@googlemail.com>
2019-03-26 16:12:18 +0000

    implement simple FizzBuzz game runner + test

---
+++ src/main/java/dev/samdjstevens/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,49 @@
+package dev.samdjstevens.fizzbuzz;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class FizzBuzz {
+
+    /**
+     * Run the FizzBuzz game up to a given number of turns, and return the
+     * comma separated string of all outputs.
+     *
+     * @param totalTurns The number of turns to run the game for.
+     * @return The string of all outputs from the turns.
+     */
+    public String run(int totalTurns) {
+        // Create a stream of integers, 0...totalTurns -1, and
+        // map to the string output counterpart, before collecting
+        // and reducing to a comma separated string.
+        return IntStream.range(0, totalTurns)
+                .mapToObj(i -> getStringForNumber(i + 1))
+                .collect(Collectors.joining(", "));
+    }
+
+    /**
+     * Returns the string to be outputted for a given integer.
+     *
+     * @param number The number we want the output for, e.g. 1, 2, 3, etc.
+     * @return The string to output for the number, e.g. "1", "two", "Fizz", etc.
+     */
+    private String getStringForNumber(int number) {
+        // If divisible by both 3 and 5, return FizzBuzz
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        }
+
+        // If divisible by 3, return Fizz
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
+
+        // If divisible by 5, return Buzz
+        if (number % 5 == 0) {
+            return "Buzz";
+        }
+
+        // Otherwise, return a string version of the number
+        return String.valueOf(number);
+    }
+}
---
+++ src/test/java/dev/samdjstevens/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,20 @@
+package dev.samdjstevens.fizzbuzz;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class FizzBuzzTest {
+
+    @Test
+    public void testFizz() {
+        FizzBuzz game = new FizzBuzz();
+        assertEquals(this.getCorrectOutputUpTo30(), game.run(30));
+    }
+
+    /**
+     * Get the correct output of the game when running for 30 turns.
+     */
+    private String getCorrectOutputUpTo30() {
+        return "1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, Buzz, 26, Fizz, 28, 29, FizzBuzz";
+    }
+}
